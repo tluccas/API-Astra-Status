3c6296e740841e8b0feedc1ee4a489a9178343c5
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-09-30 20:04:17 -0300

    first commit

---
+++ src/main/java/com/alvesdev/Astra_Status/AstraStatusApplication.java
@@ -0,0 +1,13 @@
+package com.alvesdev.Astra_Status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstraStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstraStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvesdev/Astra_Status/AstraStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesdev.Astra_Status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
