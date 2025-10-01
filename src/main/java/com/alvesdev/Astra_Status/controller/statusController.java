5870d257b96fa5a4541d0af27391480851b8a818
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-09-30 22:22:16 -0300

    refactor(dir): controller directory

---
+++ src/main/java/com/alvesdev/Astra_Status/controller/statusController.java
@@ -0,0 +1,35 @@
+package com.alvesdev.Astra_Status.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(value = "/astra")
+public class statusController {
+
+    private volatile LocalDateTime lastUpdate;
+    @PostMapping("/up-status")
+    public void isConnected() {
+        lastUpdate = LocalDateTime.now();
+        System.out.println(lastUpdate);
+    }
+
+    @GetMapping("/get-status")
+    public String getStatus() {
+        if(lastUpdate == null) {
+            return "offline";
+        }
+
+        Duration duration = Duration.between(lastUpdate, LocalDateTime.now());
+
+        if(duration.toMinutes() > 20) {
+            return "offline";
+        }
+        return "online";
+    }
+}
--- src/main/java/controller/statusController.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/astra")
-public class statusController {
-}
